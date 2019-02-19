848056e6d6ee2c3cf31479140c768eac48cc5181
sabrefencer <46977196+sabrefencer@users.noreply.github.com>
2019-02-19 15:58:48 -0500

    Update pendulum_project.py

--- pendulum_project.py
+++ pendulum_project.py
@@ -1,1 +1,6 @@
-import microbit
+import math
+
+def update_system(acc, pos, vel, time1, time2):
+    dt = time2 - time1
+    newPos = initial_position * math.asin(math.sqrt(9.8/0.20)*dt
+    newVel =
