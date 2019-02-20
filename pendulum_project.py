8c3b6c9df2ae4392a49b4185f52a63c73b64384e
wreid01 <47088472+wreid01@users.noreply.github.com>
2019-02-20 16:10:54 -0500

    Update pendulum_project.py

--- pendulum_project.py
+++ pendulum_project.py
@@ -4,10 +4,11 @@ import numpy as np
 
 #length of the pendulum is 30 cm
 #acceleration due to gravity is 9.8 m/s^2
+#position is in radians
 
 def update_system(acc, pos, vel, time1, time2):
     dt = time2 - time1
-    newPos = initial_position * math.asin(math.sqrt(9.8/0.30)*dt)
+    newPos = initial_position * math.sin(math.sqrt(9.8/0.30)*dt)
     newVel = math.sqrt(0.02*acc)
     return newPos, newVel
 
@@ -17,16 +18,19 @@ def print_system(pos, time, vel):
     print("Velocity: ", vel)
 
 #initial conditions
+initial_position = 0.5236
 pos = [0.5236] #30 degrees in radians
 vel = [0]
 acc = [5.13128, 4.27605, 3.402, 2.5656, 1.7101, 0.855148, 0, 0.855148, 1.7101, 2.5656, 3.402, 4.27605, 5.13128]
-time = np.linspace(0, 13, 14)
+time = np.linspace(0, 1.09932, 14)
 print_system(pos[0], time[0], vel[0])
 
 
-i = i
-while i < len(time)
-    newPos, newVel = update_system(acc[i], pos[i-1], vel[i-1], time[i-1], time[i])
+i = 1
+while i < len(time):
+    newPos, newVel = update_system(acc[i], pos[i-1], vel[i-1], time[0], time[i])
     pos.append(newPos)
     vel.append(newVel)
-    print_system(
+    print_system(pos[i], time[i], vel[i])
+    i += 1
+
