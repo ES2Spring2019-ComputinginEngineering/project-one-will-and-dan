46bfd5543e090c7675630c2e7ca28c33d631e33b
wreid01 <47088472+wreid01@users.noreply.github.com>
2019-02-21 16:11:55 -0500

    Update pendulum_project.py

--- pendulum_project.py
+++ pendulum_project.py
@@ -5,32 +5,35 @@ import numpy as np
 #length of the pendulum is 30 cm
 #acceleration due to gravity is 9.8 m/s^2
 #position is in radians
+#acceleration = g*cos(pi/2 - theta)
+#alpha = a/L
 
-def update_system(acc, pos, vel, time1, time2):
+
+def update_system(angAcc, theta, angVel, time1, time2):
     dt = time2 - time1
-    newPos = initial_position * math.sin(math.sqrt(9.8/0.30)*dt)
-    newVel = math.sqrt(0.02*acc)
+    newPos = theta + dt * angVel
+    newVel = angVel + dt * angAcc
     return newPos, newVel
 
-def print_system(pos, time, vel):
+def print_system(theta, time, angVel):
     print("Time: ", round(time, 3))
-    print("Position: ", round(pos, 3))
-    print("Velocity: ", round(vel, 3))
+    print("Position: ", round(theta, 3))
+    print("Velocity: ", round(angVel, 3))
 
 #initial conditions
-initial_position = 0.5236
-pos = [0.5236] #30 degrees in radians
-vel = [0]
-acc = [5.13128, 4.27605, 3.402, 2.5656, 1.7101, 0.855148, 0, 0.855148, 1.7101, 2.5656, 3.402, 4.27605, 5.13128]
+theta = [0.5236] #30 degrees in radians
+angVel = [0]
+#acc = [5.13128, 4.27605, 3.402, 2.5656, 1.7101, 0.855148, 0, 0.855148, 1.7101, 2.5656, 3.402, 4.27605, 5.13128]
 time = np.linspace(0, 1.09932, 14)
-print_system(pos[0], time[0], vel[0])
+print_system(theta[0], time[0], angVel[0])
 
 
 i = 1
 while i < len(time):
-    newPos, newVel = update_system(acc[i], pos[i-1], vel[i-1], time[0], time[i])
-    pos.append(newPos)
-    vel.append(newVel)
-    print_system(pos[i], time[i], vel[i])
+    angAcc = -9.8 * (math.cos(math.pi/2 - theta[i-1])/0.30
+    newPos, newVel = update_system(angAcc, theta[i-1], angVel[i-1], time[i-1], time[i])
+    theta.append(newPos)
+    angVel.append(newVel)
+    print_system(theta[i], time[i], angVel[i])
     i += 1
 
