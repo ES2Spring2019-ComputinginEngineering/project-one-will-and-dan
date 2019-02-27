9fa6ce4f898d8a7251a281e246705a89a58b32a4
sabrefencer <46977196+sabrefencer@users.noreply.github.com>
2019-02-27 15:01:18 -0500

    Update pendulum_project.py

--- pendulum_project.py
+++ pendulum_project.py
@@ -25,7 +25,7 @@ def print_system(theta, time, angVel):
 theta = [0.5236] #30 degrees in radians
 angVel = [0]
 #acc = [5.13128, 4.27605, 3.402, 2.5656, 1.7101, 0.855148, 0, 0.855148, 1.7101, 2.5656, 3.402, 4.27605, 5.13128]
-time = np.linspace(0, 1.09932, 14)
+time = np.linspace(0, 10, 10000)
 print_system(theta[0], time[0], angVel[0])
 
 
@@ -38,4 +38,30 @@ while i < len(time):
     print_system(theta[i], time[i], angVel[i])
     i += 1
 
+plt.figure(figsize=(4,6))
 plt.subplot(3,1,1)
+plt.plot(time, theta, 'r--')
+
+plt.xlabel('Time (seconds)')
+plt.ylabel('Position (m)')
+plt.title('Position vs Time')
+plt.xlim((0, 20)) # set x range to -1 to 8
+plt.grid()
+
+plt.subplot(3,1,2)
+plt.plot(time, angVel, 'r--') 
+plt.xlabel('Time (seconds)')
+plt.ylabel('Velocity (m/s)')
+plt.title('Velocity vs Time')
+plt.xlim((0, 20)) # set x range to -1 to 8
+plt.grid()
+
+plt.subplot(3,1,3)
+plt.plot(time, angAcc, 'ro--') 
+plt.xlabel('Time (seconds)')
+plt.ylabel('Acceleration (m/s^2)')
+plt.title('Acceleration vs Time')
+plt.xlim((0, 20)) # set x range to -1 to 8
+plt.grid()
+plt.tight_layout()
+plt.show()
