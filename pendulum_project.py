154cf43c330e75e5b2c66bc912236c8af6f38322
wreid01 <47088472+wreid01@users.noreply.github.com>
2019-02-27 15:13:25 -0500

    Update pendulum_project.py

--- pendulum_project.py
+++ pendulum_project.py
@@ -24,17 +24,19 @@ def print_system(theta, time, angVel):
 #initial conditions
 theta = [0.5236] #30 degrees in radians
 angVel = [0]
-#acc = [5.13128, 4.27605, 3.402, 2.5656, 1.7101, 0.855148, 0, 0.855148, 1.7101, 2.5656, 3.402, 4.27605, 5.13128]
+angAcc = [0]
+
 time = np.linspace(0, 10, 10000)
 print_system(theta[0], time[0], angVel[0])
 
 
 i = 1
 while i < len(time):
-    angAcc = -9.8 * (math.cos(math.pi/2 - theta[i-1])/0.30)
-    newPos, newVel = update_system(angAcc, theta[i-1], angVel[i-1], time[i-1], time[i])
+    newAcc = -9.8 * (math.cos(math.pi/2 - theta[i-1])/0.30)
+    newPos, newVel = update_system(newAcc, theta[i-1], angVel[i-1], time[i-1], time[i])
     theta.append(newPos)
     angVel.append(newVel)
+    angAcc.append(newAcc)
     print_system(theta[i], time[i], angVel[i])
     i += 1
 
