57622efde5b2a1227be071bb3912e859858f5572
wreid01 <47088472+wreid01@users.noreply.github.com>
2019-03-04 15:36:17 -0500

    Update sensor_data.py

--- sensor_data.py
+++ sensor_data.py
@@ -11,18 +11,22 @@ import random as r
 filename = "data_" + str(r.randint(1, 999)) + ".txt"
 
 f = open(filename, 'w')
+
 time0 = microbit.running_time()
-elapsed_time = time1 - time0
 
 while True:
     microbit.sleep(200)
 
     time1 = microbit.running_time()
 
+    elapsed_time = time1 - time0
+
     x = (microbit.accelerometer.get_x())
 
     y = (microbit.accelerometer.get_y())
 
-    accel = "X accel: " + str(x) + ", Y accel: " + str(y) + ", Time: " + str(elapsed_time) + "\n"
+    z = (microbit.accelerometer.get_z())
+
+    accel =  str(elapsed_time) + "\t" + str(x) + "\t" + str(y) + "\t" + str(z) + "\n"
 
     f.write(accel)
