004d0163f82b12d9463ed64bd0c3faf8fc6a9a85
wreid01 <47088472+wreid01@users.noreply.github.com>
2019-03-04 15:57:46 -0500

    Update

---
+++ data_parsing.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  4 15:30:50 2019
+
+@author: william
+"""
+import math
+
+
+fin = open("data_535.txt")
+
+list_lines = []
+time = []
+accX = []
+accY = []
+accZ = []
+angle = []
+
+for line in fin:
+    #list_lines.append(line.split("\t"))
+    time.append(int(line.split("\t")[0]))
+    accX.append(int(line.split("\t")[1]))
+    accY.append(int(line.split("\t")[2]))
+    accZ.append(int(line.split("\t")[3]))    
+    angle.append(math.tan(int(line.split("\t")[1])/int(line.split("\t")[2])))
